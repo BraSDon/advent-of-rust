16122e20170f85e1f88fa1440fe5fbbdc5edac4b
Brandon <brandon@schuehlein.eu>
2023-12-20 16:25:04 +0100

    add day01

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,120 +1,72 @@
-fn get_digits_part1(s: &str) -> (u8, u8) {
-    let mut first = None;
-    let mut last = None;
-
-    for c in s.chars() {
-        if c.is_ascii_digit() {
-            if first.is_none() {
-                first = Some(c.to_digit(10).unwrap() as u8);
-            }
-
-            last = Some(c.to_digit(10).unwrap() as u8);
-        }
-    }
-
-    (first.unwrap(), last.unwrap())
+fn find_digits(line: &str) -> Vec<u32> {
+    return line
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
 }
 
-fn end_matches(window: u64, s: &str) -> bool {
-    let s_bytes = s.as_bytes();
-    let s_len = s_bytes.len();
-
-    // Convert s to a u64, aligned to the right
-    let mut s_val: u64 = 0;
-    for &b in s_bytes {
-        s_val = (s_val << 8) | (b as u64);
-    }
-
-    let mask = (1u64 << (8 * s_len)) - 1;
-    (window & mask) == s_val
+fn combine_first_and_last(digits: Vec<u32>) -> u32 {
+    let first = digits[0].to_string();
+    let last = digits[digits.len() - 1].to_string();
+    format!("{}{}", first, last).parse::<u32>().unwrap()
 }
 
-fn get_digits_part2(s: &str) -> (u8, u8) {
-    let mut first = None;
-    let mut last = None;
+fn replace(line: &str) -> String {
+    let replacements = [
+        ("one", "o1ne"),
+        ("two", "t2wo"),
+        ("three", "t3hree"),
+        ("four", "f4our"),
+        ("five", "f5ive"),
+        ("six", "s6ix"),
+        ("seven", "s7even"),
+        ("eight", "e8ight"),
+        ("nine", "n9ne"),
+    ];
 
-    let get_digit = |c: u8, window: u64| -> Option<u8> {
-        if c == b'1' || end_matches(window, "one") {
-            Some(1)
-        } else if c == b'2' || end_matches(window, "two") {
-            Some(2)
-        } else if c == b'3' || end_matches(window, "three") {
-            Some(3)
-        } else if c == b'4' || end_matches(window, "four") {
-            Some(4)
-        } else if c == b'5' || end_matches(window, "five") {
-            Some(5)
-        } else if c == b'6' || end_matches(window, "six") {
-            Some(6)
-        } else if c == b'7' || end_matches(window, "seven") {
-            Some(7)
-        } else if c == b'8' || end_matches(window, "eight") {
-            Some(8)
-        } else if c == b'9' || end_matches(window, "nine") {
-            Some(9)
-        } else {
-            None
-        }
-    };
+    let mut result = line.to_string();
 
-    let mut window: u64 = 0;
-
-    for c in s.bytes() {
-        window = (window << 8) | (c as u64);
-
-        if let Some(d) = get_digit(c, window) {
-            if first.is_none() {
-                first = Some(d)
-            }
-
-            last = Some(d);
-        }
+    for &(original, replacement) in &replacements {
+        result = result.replace(original, replacement);
     }
 
-    (first.unwrap(), last.unwrap())
+    result
 }
 
 pub fn part_one(input: &str) -> u32 {
     input
         .lines()
-        .map(|line| {
-            let (f, l) = get_digits_part1(line);
-            f as u32 * 10 + l as u32
-        })
+        .map(find_digits)
+        .map(combine_first_and_last)
         .sum()
 }
 
 pub fn part_two(input: &str) -> u32 {
     input
         .lines()
-        .map(|line| {
-            let (f, l) = get_digits_part2(line);
-            f as u32 * 10 + l as u32
-        })
+        .map(replace)
+        .map(|line| find_digits(&line))
+        .map(combine_first_and_last)
         .sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{read_file, read_file_with_name};
+    use crate::read_file;
 
     use super::*;
 
     #[test]
     fn test_part_one() {
-        let input = read_file_with_name("examples", "01_1");
-        assert_eq!(part_one(&input), 142);
-
         let input = read_file("inputs", 1);
-        assert_eq!(part_one(&input), 54968);
+        assert_eq!(part_one(&input), 54630);
+
     }
 
     #[test]
     fn test_part_two() {
-        let input = read_file_with_name("examples", "01_2");
-        assert_eq!(part_two(&input), 281);
-
         let input = read_file("inputs", 1);
-        assert_eq!(part_two(&input), 54094);
+        assert_eq!(part_two(&input), 54770);
     }
 }
